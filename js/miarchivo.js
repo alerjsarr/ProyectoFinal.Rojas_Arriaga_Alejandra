fa5c6a71e32b4ef7227c8e8a45913465cdb8867a
alerjs <rojas.arriaga.alejandra@gmail.com>
2022-05-31 22:04:32 -0500

    creacion de array

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,7 +1,9 @@
 //Algoritmo condicional y algoritmo con ciclo para calcular costo total de productos seleccionados por el usuario
 
-const precio = 185
+/*const precio = 185
+
 let cantidadClases = Number(prompt('Escribe el número de clases que deseas adquirir'))
+
 function preciototal(precioFinal){
     alert('Tu total es de: $' + precioFinal + 'MXN')
 }
@@ -33,9 +35,33 @@ switch (cantidadClases){
         alert('Tu total es de: $5,500 MXN')
         break
 
+}*/
+
+//Arrays
+const clases5 = { 
+    promo: 'Opción 1', 
+    precio:'$850 MXN'}
+const clases10 = {
+    promo: 'Opción 2', 
+    precio:'$1,400 MXN'}
+const clases15 = {
+    promo: 'Opción 3', 
+    precio:'$1,950 MXN'}
+const clases25 = {
+    promo: 'Opción 4', 
+    precio:'$3,150 MXN'}
+const clases50 = {
+    promo: 'Opción 5', 
+    precio:'$5,500 MXN'}
+
+const clasesPromo = [clases5,clases10,clases15,clases25,clases50]
+
+console.log(clasesPromo);
+
+for (const clasePromo of clasesPromo){
+console.log(clasePromo.promo);
+console.log(clasePromo.precio);
 }
-
-
 
 
 
