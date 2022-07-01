a59c72bec30c5bf3ae10ca9cf4a44273cfb93b39
alerjs <rojas.arriaga.alejandra@gmail.com>
2022-07-01 03:30:39 -0500

    ejemplo fetch async-await

--- js/practicas.js
+++ js/practicas.js
@@ -63,18 +63,18 @@
 //puede ser ruta absoluta con URL o relativa con archivo interno
 
 //FETCH
-// let dataDeApi 
+let dataDeApi 
 
-// fetch('https://swapi.dev/api/people/4/')
-//     .then((response)=> response.json())
-//     .then((data) => {
-//         dataDeApi= data
-//         localStorage.setItem('data', data)
-//         console.log(dataDeApi)
-//     })
-//     .catch((error)=>{
-//         console.log(error);
-//     })
+fetch('https://swapi.dev/api/people/4/')
+    .then((response)=> response.json())
+    .then((data) => {
+        dataDeApi= data
+        localStorage.setItem('data', data)
+        console.log(dataDeApi)
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 //1.sale que la promesa está en 'pending' porque el log es sincronico. 
 //2.le ponemos un then para que una vcez que el fetch traiga la info, darle la orden de que hacer con ella, en este caso un log con la peticion. 
 //3. no se ve la info, si no un objeto tipo response.
@@ -91,32 +91,41 @@
 //es un fetch con method, headers y body. Nos regresa un id! 
 //podrias poner tus opciones de clases, remplaza mi page de data?
 
-// fetch ('https://jsonplaceholder.typicode.com/posts',{
-//     method: 'POST',
-//     body: JSON.stringify({
-//         title: 'PAQUETES DE CLASES',
-//         body: 'post de pueba',
-//         userId: 1,
-//     }),
-//     headers: {
-//         'Content-type':'application/json;charset=UTF-8',
-//     },
-// })
-// .then ((response)=> response.json())
-// .then ((data)=> console.log(data))
+fetch ('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'PAQUETES DE CLASES',
+        body: 'post de pueba',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type':'application/json;charset=UTF-8',
+    },
+})
+.then ((response)=> response.json())
+.then ((data)=> console.log(data))
 
 //RUTAS RELATIVAS FETCH GET RELATIVO
 //fetch a un archivo interno con .json
 //*nota: no se puede comentar en json ggg
 //en tu archivo de json se simula un array o un objeto o un array de objetos... RECUERDA CON 2BLE COMILLA
 
-fetch('../paquetes.json')
+// fetch('../paquetes.json')
 
-.then ((response) => response.json())
-.then ((data)=> console.log(data))
-//alo? porque no agarra?
+// .then ((response) => response.json())
+// .then ((data)=> console.log(data))
 
- 
+
+//ASYNC-AWAIT
+
+const pedidoApi= async ()=>{
+const response= await fetch ('../paquetes.json')
+const data = await response.json()
+console.log(data);
+}
+pedidoApi()
+//pasa a ser sincronico 
+
 
 
 
